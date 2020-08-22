6b9c3b1ba0451f5cd1796d50a5b7f54ec7381b62
JaeHo Song <thruthesky@gmail.com>
2020-08-22 23:23:29 +0800

    update for pwa

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/**
+ * @file index.js
+ */
+/**
+ * Definitions
+ */
+const theme_path = '/wp-content/themes/cms';
+/**
+ * Initialization
+ */
+initServiceWorker();
+
+
+
+
+function initServiceWorker() {
+
+// Check that service workers are supported
+    if ('serviceWorker' in navigator) {
+        alert('supported?');
+        // Use the window load event to keep the page load performant
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register(theme_path + '/js/service-worker.js').then(function(registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    } else {
+        console.log('service worker is not supported');
+    }
+}
