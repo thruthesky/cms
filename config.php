fdd6ca0991806865283711dde0d92d2dc1385137
JaeHo Song <thruthesky@gmail.com>
2020-08-24 21:03:05 +0800

    update on multitheme based on domain

--- config.php
+++ config.php
@@ -5,3 +5,11 @@ class Config {
     static public $apiUrl = '/wp-content/themes/cms/api.php';
     static public $registerPage = '/?page=user.register';
 }
+
+$_host = $_SERVER['HTTP_HOST'];
+
+if ($_host == 'wp-blog.philgo.com' ) {
+    Config::$domain = 'blog';
+} else if ($_host == 'wp-realestate.philgo.com' ) {
+    Config::$domain = 'realestate';
+}
---
+++ pages/realestate/index.php
@@ -0,0 +1,3 @@
+<h1>Real Estate</h1>
+
+This is realestate theme.
