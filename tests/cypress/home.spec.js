3f4e6c11aed7c806453ca9f8ee572ed25d29e573
JAEHO SONG <thruthesky@gmail.com>
2020-09-14 21:06:43 +0900

    push:live

--- tests/cypress/home.spec.js
+++ tests/cypress/home.spec.js
@@ -23,34 +23,34 @@ context('Integration Test', () => {
             cy.get("[data-page='home']");
         })
     })
-    describe('Register', () => {
-        it('Submit register', () => {
-            cy.get("[data-button='register']").click();
-            cy.get("form.register [name='user_email']").type(faker.internet.email());
-            cy.get("form.register [name='user_pass']").type(faker.internet.password());
-            cy.get("form.register [name='first_name']").type(faker.name.findName());
-            cy.get("form.register [name='middle_name']").type('J');
-            cy.get("form.register [name='last_name']").type(faker.name.lastName());
-            cy.get("form.register [name='nickname']").type(faker.name.firstName());
-            cy.get("form.register [name='mobile']").type(faker.phone.phoneNumber());
-            cy.get("form.register [data-button='submit']").click();
-        })
-    })
-    describe('Profile', () => {
-        it('Submit profile', () => {
-            cy.wait(500);
-            cy.get("[data-button='profile']").click();
-            cy.get("[data-button='profile-update']").click();
-            cy.get("form.register [name='mobile']").clear().type(faker.phone.phoneNumber());
-            cy.get("form.register [data-button='submit']").click();
-        })
-    })
-    describe('Logout', () => {
-        it('Do logout', () => {
-            cy.wait(500);
-            cy.get("[data-button='logout']").click();
-        })
-    })
+    // describe('Register', () => {
+    //     it('Submit register', () => {
+    //         cy.get("[data-button='register']").click();
+    //         cy.get("form.register [name='user_email']").type(faker.internet.email());
+    //         cy.get("form.register [name='user_pass']").type(faker.internet.password());
+    //         cy.get("form.register [name='first_name']").type(faker.name.findName());
+    //         cy.get("form.register [name='middle_name']").type('J');
+    //         cy.get("form.register [name='last_name']").type(faker.name.lastName());
+    //         cy.get("form.register [name='nickname']").type(faker.name.firstName());
+    //         cy.get("form.register [name='mobile']").type(faker.phone.phoneNumber());
+    //         cy.get("form.register [data-button='submit']").click();
+    //     })
+    // })
+    // describe('Profile', () => {
+    //     it('Submit profile', () => {
+    //         cy.wait(500);
+    //         cy.get("[data-button='profile']").click();
+    //         cy.get("[data-button='profile-update']").click();
+    //         cy.get("form.register [name='mobile']").clear().type(faker.phone.phoneNumber());
+    //         cy.get("form.register [data-button='submit']").click();
+    //     })
+    // })
+    // describe('Logout', () => {
+    //     it('Do logout', () => {
+    //         cy.wait(500);
+    //         cy.get("[data-button='logout']").click();
+    //     })
+    // })
 
 
 })
