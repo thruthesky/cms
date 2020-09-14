ba883e07393c7b3cf1522ad17a6175cbe1352ac6
lancelynyrd <pinedaclp@gmail.com>
2020-09-14 20:01:52 +0800

    post list under post view

--- pages/default/post/view.php
+++ pages/default/post/view.php
@@ -1,4 +1,6 @@
 <?php
 
+$forum = get_forum_setting();
+
 include widget(forum(POST_VIEW_THEME));
-include widget(forum(POST_LIST_THEME));
+if (isset($forum['post_list_under_view']) && $forum['post_list_under_view'] === 'Y') include widget(forum(POST_LIST_THEME));
