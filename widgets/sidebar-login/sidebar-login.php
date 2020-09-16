63f41046bc308336b91eee4e60e6a05a980e991c
JAEHO SONG <thruthesky@gmail.com>
2020-09-16 14:04:57 +0900

    update for login fix

--- widgets/sidebar-login/sidebar-login.php
+++ widgets/sidebar-login/sidebar-login.php
@@ -18,24 +18,28 @@
 
 
 
+<div class="login-form" style="display: none;">
 
-<form class="login login-form" onsubmit="return onLoginFormSubmit(this)" style="display: none;">
-    <div class="mb-3">
-        <label class="form-label">Email address</label>
-        <input type="email" class="form-control"aria-describedby="emailHelp" name="user_email">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Password</label>
-        <input type="password" class="form-control" name="user_pass">
-    </div>
-    <div class="mb-3 form-check">
-        <label class="form-check-label">
-            <input type="checkbox" class="form-check-input" name="remember" value="on">
-            Remember Me</label>
-    </div>
-    <div class="d-flex justify-content-between">
-        <button type="submit" class="btn btn-primary">LOGIN</button>
-        <a class="btn btn-secondary" data-button="register" href="<?php echo Config::$registerPage?>">Register</a>
-    </div>
-</form>
+    <form onsubmit="return onLoginFormSubmit(this)">
+        <div class="mb-3">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control"aria-describedby="emailHelp" name="user_email">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control" name="user_pass">
+        </div>
+        <div class="mb-3 form-check">
+            <label class="form-check-label">
+                <input type="checkbox" class="form-check-input" name="remember" value="on">
+                Remember Me</label>
+        </div>
+        <div class="d-flex justify-content-between">
+            <button type="submit" class="btn btn-primary">LOGIN</button>
+            <a class="btn btn-secondary" data-button="register" href="<?php echo Config::$registerPage?>">Register</a>
+        </div>
+    </form>
 
+    <?php include widget('social-login/buttons'); ?>
+
+</div>
