f9f79ffc6cc2c3e9bb12bf00dd7d99fe5156b27f
JAEHO SONG <thruthesky@gmail.com>
2020-09-17 23:02:51 +0900

    push:live

--- php/defines.php
+++ php/defines.php
@@ -30,7 +30,8 @@ define('POST_EDIT_THEME', 'post_edit_theme');
 define('THEME_PATH', ABSPATH . 'wp-content/themes/cms');
 define('THEME_URL', '/wp-content/themes/cms');
 /// HOME_URL is different from Wordpress Home Url which is based on database setting.
-define('HOME_URL', "https://$_SERVER[HTTP_HOST]");
+$__domain = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
+define('HOME_URL', "https://$__domain");
 define('FULL_THEME_URL', HOME_URL . THEME_URL);
 define('ANONYMOUS_PROFILE_PHOTO', '/wp-content/themes/cms/img/anonymous/anonymous.jpg');
 
