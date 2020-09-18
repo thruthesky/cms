de786a24c72975771bc1fb5b1e274d3742a3dba2
lancelynyrd <pinedaclp@gmail.com>
2020-09-18 13:55:11 +0800

    fix error

--- functions.php
+++ functions.php
@@ -933,10 +933,10 @@ function insert_forum_settings_as_javascript_into_header() {
 	$re = $__get_forum_setting;
 
 
-	$post_show_like = isset($re[POST_SHOW_LIKE]) ? $re['POST_SHOW_LIKE'] : '';
-	$post_show_dislike = isset($re[POST_SHOW_DISLIKE]) ? $re['POST_SHOW_DISLIKE'] : '';
-	$comment_show_like = isset($re[COMMENT_SHOW_LIKE]) ? $re['COMMENT_SHOW_LIKE'] : '';
-	$comment_show_dislike = isset($re[COMMENT_SHOW_DISLIKE]) ? $re['COMMENT_SHOW_DISLIKE'] : '';
+	$post_show_like = isset($re[POST_SHOW_LIKE]) ? $re[POST_SHOW_LIKE] : '';
+	$post_show_dislike = isset($re[POST_SHOW_DISLIKE]) ? $re[POST_SHOW_DISLIKE] : '';
+	$comment_show_like = isset($re[COMMENT_SHOW_LIKE]) ? $re[COMMENT_SHOW_LIKE] : '';
+	$comment_show_dislike = isset($re[COMMENT_SHOW_DISLIKE]) ? $re[COMMENT_SHOW_DISLIKE] : '';
 	$__head_script .= <<<EOS
 <script>
 	const forum = {
