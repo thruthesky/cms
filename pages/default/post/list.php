47df4b2634daa6c4ce687077214f72517e7e2a45
JAEHO SONG <thruthesky@gmail.com>
2020-09-22 20:43:03 +0900

    update for error

--- pages/default/post/list.php
+++ pages/default/post/list.php
@@ -1,6 +1,6 @@
 <?php
 if ( !forum('cat_ID') ) {
-	return include page('error.wrong-input', ['error' => 'Forum slug is incorrect or it may not created.']);
+	return include page('error.display', ['title'=>'Forum slug error', 'body' => 'Forum slug is incorrect or it may not created.']);
 }
 include widget(forum(POST_LIST_THEME, 'post.list'));
 
