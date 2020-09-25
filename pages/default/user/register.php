d699bd5cdb888593a3a1525d994c1cf5c57724ad
lancelynyrd <pinedaclp@gmail.com>
2020-09-25 13:37:47 +0800

    update registration page

--- pages/default/user/register.php
+++ pages/default/user/register.php
@@ -9,32 +9,25 @@ if ( !loggedIn() && in('mobile') == null && Config::$verifyMobileOnRegistration 
 /**
  * Init
  */
-?>
-<script>
-    localStorage.removeItem('mobile')
-</script>
+if( !Config::$verifiedMobileOnly ) { ?>
+    <script>
+        localStorage.removeItem('mobile')
+    </script>
+<?php } ?>
 
 <div id="register-page" class="px-40 mt-60">
     <div class="fs-20 black"><?=tr(REGISTRATION_HEAD)?></div>
-    <h1  class="fs-40 font-weight-bold mb-34 black"><?= loggedIn() ? tr([en=>"Profile", ko =>'회원 정보 수정']) : tr([en=>"Registration", ko =>'회원 가입'])?></h1>
+    <h1  class="fs-40 font-weight-bold mb-34 black"><?=tr([en=>"Registration", ko =>'회원 가입'])?></h1>
     <form class="mb-56" id="register-form" onsubmit="return onRegisterFormSubmit()">
-        <?php if (loggedIn()) { ?>
-        <input type="hidden" name="session_id" value="<?=login('session_id')?>">
-        <?php } else { ?>
         <input type="hidden" name="firebase_uid" value="">
-            <script>
-                $$(function() {
-                    firebaseAuth(function(user) {
-                        $('[name="firebase_uid"]').val(user.uid);
-                    });
-                })
-            </script>
-        <?php } ?>
-        <?php if (loggedIn()) { ?>
-        <div class="mb-34">
-            <? include 'form-profile-photo.php'?>
-        </div>
-        <?php } ?>
+        <script>
+            $$(function() {
+                firebaseAuth(function(user) {
+                    $('[name="firebase_uid"]').val(user.uid);
+                });
+            })
+        </script>
+
         <?php if ( login(SOCIAL_LOGIN) == null ) { ?>
             <label class="form-label fs-14 gray100"><?=tr(emailAddress)?></label>
             <div class="input-group mb-34">
@@ -42,17 +35,14 @@ if ( !loggedIn() && in('mobile') == null && Config::$verifyMobileOnRegistration 
                 <div class="input-group-append">
                     <span class="input-group-text smat-input-group-text"><i class="fa fa-user fs-xl"></i></span>
                 </div>
-<!--                <small class="form-text text-muted">--><?//=tr(emailAddressDescription)?><!--</small>-->
             </div>
-            <? if (!loggedIn()) { ?>
-                <label class="form-label fs-14 gray100"><?=tr(PASSWORD)?></label>
-                <div class="input-group mb-34">
-                    <input type="password" class="form-control smat-input" name="user_pass">
-                    <div class="input-group-append show pointer" onclick="showPassword()">
-                        <span class="input-group-text smat-input-group-text"><i class="fa fa-eye-slash fs-lg"></i></span>
-                    </div>
+            <label class="form-label fs-14 gray100"><?=tr(PASSWORD)?></label>
+            <div class="input-group mb-34">
+                <input type="password" class="form-control smat-input" name="user_pass">
+                <div class="input-group-append show pointer" onclick="showPassword()">
+                    <span class="input-group-text smat-input-group-text"><i class="fa fa-eye-slash fs-lg"></i></span>
                 </div>
-            <?}?>
+            </div>
         <?php } ?>
 
 
@@ -68,26 +58,20 @@ if ( !loggedIn() && in('mobile') == null && Config::$verifyMobileOnRegistration 
             <div class="mb-34">
                 <label class="form-label fs-14 gray100"><?=tr('mobileNo')?></label>
                 <div>
-                    <?php if ( loggedIn() ) echo login('mobile'); else {?>
 
-                        <?php if ( Config::$verifiedMobileOnly ) { ?>
+                    <?php if ( Config::$verifiedMobileOnly ) { ?>
 
-                            <div class="mobile"></div>
-                        <input type="hidden" name="mobile" value="">
-                            <script>
-                                $$(function() {
-                                    $('.mobile').text(localStorage.getItem('mobile'))
-                                    $('[name="mobile"]').val(localStorage.getItem('mobile'))
-                                })
-                            </script>
-
-                        <?php } else { ?>
-                        <input class="form-control smat-input" type="text" name="mobile" value="">
-                        <?php }?>
-
-
-                    <?php } ?>
-
+                        <div class="mobile"></div>
+                    <input type="hidden" name="mobile" value="">
+                        <script>
+                            $$(function() {
+                                $('.mobile').text(localStorage.getItem('mobile'))
+                                $('[name="mobile"]').val(localStorage.getItem('mobile'))
+                            })
+                        </script>
+                    <?php } else { ?>
+                    <input class="form-control smat-input" type="text" name="mobile" value="">
+                    <?php }?>
                 </div>
             </div>
         <?php } ?>
@@ -103,21 +87,20 @@ if ( !loggedIn() && in('mobile') == null && Config::$verifyMobileOnRegistration 
                 ])?></div>
         </div>
     </div>
+
     <div class="mb-56">
         <?php include widget('user.logged-with') ?>
     </div>
-    <?php
-    if ( ! loggedIn() ) { ?>
+
     <div class="mb-56 text-center" style="height: 14px; border-bottom: 1px solid #AFAFAF">
           <span class="px-10 bg-white lightgray">
             <?=tr('or')?>
           </span>
     </div>
+
     <div class="mb-56">
         <?php include widget('social-login/buttons') ?>
     </div>
-
-    <? } ?>
 
 </div>
 
