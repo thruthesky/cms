9867d74708b45d3337937387bf172f4513258ee1
JaeHo Song <thruthesky@gmail.com>
2020-09-25 20:34:11 +0800

    desktop ui design

--- pages/default/widgets/footer/desktop-footer.php
+++ pages/default/widgets/footer/desktop-footer.php
@@ -1,8 +1,15 @@
 <?php
 ?>
 
-<div class="l-center rounded mt-5 p-3 bg-skyblue white">
+<div class="l-center mt-5 p-3 bg-light">
 
-	<div>Language: <?=get_browser_language()?></div>
-	<div>(C) All Rights Reserved by. <?=tr('appName')?></div>
+    <div class="mt-5">
+        커뮤니티: 자유게시판 질문과답변
+    </div>
+    <div class="mt-3">
+        개발강좌: 플러터 강좌 다트 강좌 파이어베이스 강좌 워드프레스 강좌 Node.js 강좌
+    </div>
+    <div class="mt-5">
+        회사소개 제휴제안 개인정보처리방침
+    </div>
 </div>
