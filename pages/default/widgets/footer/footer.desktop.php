7010f97eb7e70b6b69972714afd291c89b386750
JAEHO SONG <thruthesky@gmail.com>
2020-09-28 12:46:07 +0900

    update on desktop ui

--- pages/default/widgets/footer/footer.desktop.php
+++ pages/default/widgets/footer/footer.desktop.php
@@ -16,6 +16,14 @@
         <a href="/?page=post.list&slug=wordpress" class="link pl-2">PHP 워드프레스 강좌</a>
         <a href="/?page=post.list&slug=nodejs" class="link pl-2">Node.js 강좌</a>
     </div>
+    <div class="d-flex mt-3">
+        <div class="bold">공식문서</div>
+        <a class="link pl-4" href="https://groups.google.com/a/dartlang.org/g/announce" target="_blank">다트공지</a>
+        <a class="link pl-2" href="https://medium.com/dartlang" target="_blank">다트블로그</a>
+        <a class="link pl-2" href="https://groups.google.com/g/flutter-announce" target="_blank">플러터공지</a>
+        <a class="link pl-2" href="https://medium.com/flutter" target="_blank">플러터블로그</a>
+        <a href="https://twitter.com/flutterdev?lang=en" target="_blank" class="link pl-2">플러터트위터</a>
+    </div>
     <div class="d-flex mt-5">
         <div class="bold">회사소개</div>
         <a href="/?page=post.list&slug=discussion" class="link pl-4">제휴제안</a>
