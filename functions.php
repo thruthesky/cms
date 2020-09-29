88f992aa5761ba7b66e7a5c884f92a8c4ae2fd33
JAEHO SONG <thruthesky@gmail.com>
2020-09-29 19:51:07 +0900

    commenting on functions

--- functions.php
+++ functions.php
@@ -1257,16 +1257,22 @@ function postHasFiles($post) {
     return $post && $post['files'] && !empty($post['files']);
 }
 
-function short_name($name) {
-    if ( strlen($name) > 14 ) return mb_substr($name, 0, 14);
+function short_name($name, $len=14) {
+    if ( strlen($name) > $len ) return mb_substr($name, 0, $len);
     else return $name;
 }
 
 function short_content( $content, $length = 64 ) {
+
+	/// @TODO use html entity decode function Only if there are more characters to convert.
     $content = str_replace("&nbsp;", ' ', $content);
+
+    ///
     $content = preg_replace("/\s+/", ' ', $content);
+
+    ///
     $content = strip_tags($content);
 
-    return strlen($content) > $length ? mb_substr( $content, 0, $length ) ."&hellip;" : $content;
+    return strlen($content) > $length ? mb_substr( $content, 0, $length ) . "..." : $content;
 
 }
