4244a529cb74f36525094be6051a491ae15ef1f2
JAEHO SONG <thruthesky@gmail.com>
2020-10-02 00:22:27 +0900

    update on vue theme

--- pages/vue/widgets/header/header.php
+++ pages/vue/widgets/header/header.php
@@ -4,10 +4,9 @@
 
         <a href="/">Home</a>
         <a href="/?page=user.login" v-if="isLoggedOut">Login</a>
-        <a href="/?page=user.logout" v-if="isLoggedIn">Logout</a>
-        <a href="/?page=user.register">Register</a>
-        <a href="/?page=user.profile">Profile</a>
-
+        <a href="#" @click="logout" v-if="isLoggedIn">Logout</a>
+        <a href="/?page=user.register" v-if="isLoggedOut">Register</a>
+        <a href="/?page=user.profile" v-if="isLoggedIn">Profile</a>
         (Desktop header)
     </div>
 
@@ -15,12 +14,11 @@
 <section class="d-lg-none">
 
     <div class="flex justify-content-between">
-    <a href="/">Home</a>
+        <a href="/">Home</a>
         <a href="/?page=user.login" v-if="isLoggedOut">Login</a>
         <a href="#" @click="logout" v-if="isLoggedIn">Logout</a>
-    <a href="/?page=user.register" v-if="isLoggedOut">Register</a>
-    <a href="/?page=user.profile" v-if="isLoggedIn">Profile</a>
-
-    (Mobile Header)
+        <a href="/?page=user.register" v-if="isLoggedOut">Register</a>
+        <a href="/?page=user.profile" v-if="isLoggedIn">Profile</a>
+        (Mobile Header)
     </div>
 </section>
